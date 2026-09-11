17a6f32c53866fac391a0cf6a68058d9ac32db45
Bhushan Salkar <salkarbhushan57@gmail.com>
2026-09-11 23:53:37 +0530

    update 1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+let name = "Bhushan";
 function App() {
   return (
   <>
@@ -10,6 +10,7 @@ function App() {
   </nav>
   <div className=" container">
     <p>
+      <h1> Hello {name} </h1>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam ducimus dolores est voluptate harum aut non praesentium iure eos delectus, id accusantium voluptatum a animi nobis doloribus recusandae maxime et.
     </p>
   </div>
